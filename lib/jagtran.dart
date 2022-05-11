ef8bcbebcf18403d61bc6c3e695f9af6ba762d2a
Justin Petry <justincpetry@gmail.com>
2022-05-10 19:10:11 -0500

    bus tracking works (placeholder json only) this code fucking sucks

--- lib/jagtran.dart
+++ lib/jagtran.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'main.dart';
 
 class BusPage extends StatefulWidget {
@@ -12,25 +13,18 @@ class BusPage extends StatefulWidget {
 
   @override
   _BusPageState createState() => _BusPageState();
-  /*@override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      drawer: MainDrawer(),
-      appBar: AppBar(title: Text('JagTran')),
-    );
-  }*/
 }
 
 class _BusPageState extends State<BusPage> {
 
-  get _routes => JagTran().getBuses();
-  int id = 0;
+  get _routes => JagTran().getRoutes();
+  int _routeId = 0;
+  int _busId = 0;
+  get _map => JagTran().getMap(_busId);
 
   @override
   initState() {
     super.initState();
-    //Future<List<Route>?> _routes = JagTran().getBuses();
   }
 
   @override
@@ -38,53 +32,56 @@ class _BusPageState extends State<BusPage> {
     return Scaffold(
         drawer: MainDrawer(),
         appBar: AppBar(title: Text('JagTran')),
-        body: ListView(
-          children: [
-            map(),
-            routeListBuilder(),
-          ]
-        ),
+        body: routeListBuilder()
     );
   }
-  Image map() {
-    if (id == 0) {
-      late Image map;
-      Uint8List blankBytes = Base64Codec().decode("R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
-      map = Image.memory(blankBytes, height: 1);
-      return map;
-    }
-    else {
-      return Image.network("http://kindaconfused.com/assets/img/logo.jpg");
-    }
+  FutureBuilder<Image> mapBuilder() {
+    return FutureBuilder<Image>(
+      future: _map,
+      builder: (BuildContext context, AsyncSnapshot<Image> snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return Text("Loading..");
+        }
+        if (snapshot.hasData) {
+            return (snapshot.data as Widget);
+        } else {
+          return Container(
+              child: const Center(
+                child:
+                CircularProgressIndicator(),
+              )
+          );
+        }
+      }
+    );
   }
-  FutureBuilder<List<Route>> routeListBuilder() {
-    return FutureBuilder<List<Route>>(
+  FutureBuilder<List<Map<int, Route?>>> routeListBuilder() {
+    return FutureBuilder<List<Map<int, Route?>>>(
         future: _routes,
-        builder: (BuildContext context, AsyncSnapshot<List<Route>> snapshot) {
+        builder: (BuildContext context, AsyncSnapshot<List<Map<int, Route?>>> snapshot) {
           if (snapshot.hasData) {
-            return Container(
-              child: ListView.builder(
-                itemCount: snapshot.data?.length,
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
+            return ListView.builder(
+                itemCount: (snapshot.data?.length)! + 1,
                 itemBuilder: (context, index) {
-                  Route? currentEntry = snapshot.data?[index];
+                  if (index == 0) {
+                    index++;
+                    return mapBuilder();
+                  }
+                  Map<int, Route?>? currentEntry = snapshot.data?[index-1];
                   return ListTile(
-                      title: Text('${currentEntry?.name}'),
-                      onTap: (){
+                      title: Text('${currentEntry?.values.toList()[0]?.name}'),
+                      onTap: () {
                         if (currentEntry != null) {
-                          id = currentEntry.id;
+                          setState(() {
+                            _routeId = currentEntry?.values.toList()[0]?.id ??
+                                0;
+                            _busId = currentEntry?.keys.toList()[0] ?? 0;
+                          });
                         }
-
-                        //Navigator.push(
-                          //context,
-                          //MaterialPageRoute(builder: (context) => const MapView()),
-                        //);
                       }
                   );
-                },
-              ),
-            );
+                }
+              );
           } else {
             return Container(
                 child: const Center(
@@ -99,34 +96,92 @@ class _BusPageState extends State<BusPage> {
   }
 }
 
-/*class MapView extends StatelessWidget {
-
-  const MapView({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: (
-        Center(child: Image.network("http://kindaconfused.com/assets/img/logo.jpg")
-        )
-      )
-    );
-  }
-}*/
-
 class JagTran {
-  Future<List<Route>> getBuses() async {
-    var response = await http.get(Uri.parse("http://jagtran.doublemap.com/map/v2/routes"));
+  Future<List<Map<int, Route?>>> getRoutes() async {
     List<Route> routes;
+    /*var response = await http.get(Uri.parse("http://jagtran.doublemap.com/map/v2/routes"));
     if (response.statusCode == 200) {
-      Iterable l = json.decode(response.body);
       routes = (json.decode(response.body) as List).map((i) =>
         Route.fromJson(i)).toList();
     }
     else {
       return new List<Route>.empty();
+    }*/
+    var routesJson = await rootBundle.loadString("assets/routesOriginal.json");
+    var busesJson = await rootBundle.loadString("assets/buses.json");
+    routes = (json.decode(routesJson) as List).map((i) =>
+        Route.fromJson(i)).toList();
+    List<Bus> buses = (json.decode(busesJson) as List).map((i) =>
+        Bus.fromJson(i)).toList();
+    Map<int, Route> routeNums = {};
+    List<Map<int, Route?>> list = List.empty(growable: true);
+    for (var route in routes) {
+      routeNums[route.id] = route;
     }
-    return routes;
+    for (var bus in buses) {
+      list.add({bus.id:routeNums[bus.routeId]});
+    }
+    return list;
   }
+  Future<Map<int, int>> getBusRoutes() async {
+    // load placeholder json since no buses run in May semester
+    String busesJson = await rootBundle.loadString("assets/buses.json");
+    //List<Route> routes = (json.decode(routesJson) as List).map((i) =>
+    //    Route.fromJson(i)).toList();
+    List<Bus> buses = (json.decode(busesJson) as List).map((i) =>
+        Bus.fromJson(i)).toList();
+    Map<int, int> routeMap = Map();
+    for (Bus bus in buses) {
+      routeMap.addAll({bus.routeId:bus.id});
+    }
+    return routeMap;
+  }
+  Future<Map<int, Bus>> getBuses() async {
+    String busesJson = await rootBundle.loadString("assets/buses.json");
+    List<Bus> buses = (json.decode(busesJson) as List).map((i) =>
+        Bus.fromJson(i)).toList();
+    Map<int, Bus> busMap = Map();
+    for (Bus bus in buses) {
+      busMap[bus.id] = bus;
+    }
+    return busMap;
+  }
+  Future<Image> getMap(int busId) async {
+    Uint8List blankBytes = Base64Codec().decode("R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
+    Image placeholderImage = Image.memory(blankBytes, height: 1);
+    if (busId == 0) {
+      return placeholderImage;
+    }
+    Map<int, Bus> buses = await getBuses();
+    Bus? bus = buses[busId];
+    if (bus != null) {
+      String apikey = await rootBundle.loadString("assets/api.key");
+      return Image.network(
+          "http://open.mapquestapi.com/staticmap/v4/getmap?key=$apikey&size=500,400&zoom=16&center=${bus
+              .lat},${bus.long}&pois=blue_1,${bus.lat},${bus.long},0,0");
+    }
+    else {
+      return placeholderImage;
+    }
+  }
+}
+
+class Bus {
+  final int id;
+  final int routeId;
+  final double lat;
+  final double long;
+  const Bus({
+    required this.id,
+    required this.routeId,
+    required this.lat,
+    required this.long,
+  });
+  Bus.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        routeId = json['route'],
+        lat = json['lat'],
+        long = json['lon'];
 }
 
 class Route {
